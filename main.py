f4fba46dc720fcfad1379b2e4f040e8ac161f960
negi524 <egtnrumpbiizarre.info@gmail.com>
2021-06-06 07:55:08 +0900

    insert文生成

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import pandas as pd
 
 def main():
     """main関数
     """
+    df = pd.read_csv('./resources/sample.csv')
+
+    # CSVのヘッダーを取得
+    df_header = df.columns
+
+    table_name = 'hoge'
+    columns_list = ','.join(df_header)
+
+    result = []
+    
+    for row in df.itertuples(index=False):
+        values_list = ','.join(str(x) for x in row)
+        result.append(f'INSERT INTO {table_name} ({columns_list}) VALUES ({values_list})')
+    
+    print(result)
+
 
 if __name__ == '__main__':
     main()
